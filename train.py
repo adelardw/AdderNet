97010d7607e4993461f075befbbe535d2577c8de
Yaroslav <yaroslav.sergaev@gmail.com>
2025-04-11 12:57:57 +0300

    .

--- train.py
+++ train.py
@@ -1,9 +1,9 @@
 from lightning_modules.lightning_module import *
 from loaders import *
 from utils import *
-import yaml
 
 cfg_path = 'configs/denoise_model_v1_cfg.yaml'
+ckpt_path = '/home/ys/diploma/lightning_logs/version_6/checkpoints/best-checkpoint-epoch=21-valid_loss=42444.98.ckpt'
 train_loader, val_loader, test_loader = get_loaders(speech_dirs=["dataset/dev-clean", "dataset/test-clean.tar"],
                                                     noise_dir="dataset/wham_noise//wham_noise",
                                                     batch_size=4,
@@ -12,6 +12,6 @@ train_loader, val_loader, test_loader = get_loaders(speech_dirs=["dataset/dev-cl
 
 
 
-model = GiGaSpectrogramLightningModelUnet(**load_cfg(cfg_path))
+model = GiGaSpectrogramLightningModelUnet.load_from_checkpoint(ckpt_path,**load_cfg(cfg_path))   #(**load_cfg(cfg_path))
 trainer = L.Trainer(accelerator="auto",max_epochs=300,logger=True)                  
 trainer.fit(model, train_loader, val_loader)
