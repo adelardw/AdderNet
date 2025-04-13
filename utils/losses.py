eaca9f7082d8a11d454a3c57250bc7af2b504c15
Yaroslav <yaroslav.sergaev@gmail.com>
2025-04-13 14:12:48 +0300

    .

--- utils/losses.py
+++ utils/losses.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchaudio.transforms as T
 
 class SiSDRLoss(nn.Module):
     def __init__(self, eps: float = 1e-9):
@@ -15,7 +16,7 @@ class SiSDRLoss(nn.Module):
 
     def forward(self, output, target):
 
-        alpha = torch.sum(output * target, dim=-1,keepdim=True) / torch.norm(target, dim=-1)**2 
+        alpha = torch.sum(output * target, dim=-1,keepdim=True) / (torch.norm(target, dim=-1)**2 + self.eps)
 
         proj = alpha * target
 
@@ -24,37 +25,6 @@ class SiSDRLoss(nn.Module):
 
         return  -(10 * (torch.log10(proj_norm**2 / (diff_norm**2 + self.eps )))).mean()
 
-class SDR(nn.Module):
-
-    def __init__(self, eps = 1e-12):
-        super().__init__()
-        self.eps = eps
-    
-    def forward(self, x, y, y_hat):
-
-        alpha = torch.norm(y, dim=-1, keepdim=True)**2 / (torch.norm(y, dim=-1, keepdim=True)**2 + torch.norm(x - y, dim=-1, keepdim=True)**2 + self.eps)
-        loss_p1 = -alpha * (torch.sum(y * y_hat, dim=-1) / (torch.norm(y, dim=-1, keepdim=True) * torch.norm(y_hat, dim=-1, keepdim=True) + self.eps))
-        loss_p2 = -(torch.ones_like(alpha) - alpha) * (torch.sum((x - y) * (x - y_hat), dim=-1) / (torch.norm((x - y), dim=-1, keepdim=True) * torch.norm((x - y_hat), dim=-1, keepdim=True) + self.eps)) 
-
-        return (loss_p1 + loss_p2).mean()
-    
-
-
-class CIRMLoss(nn.Module):
-    def __init__(self, k: int | float = 10, c: float  = 0.1):
-        """
-
-        Args:
-            k (int | float): constant of compressions mask in interval [-k, k]. Defaults to 10.
-            c (float): constant of controls mask steepness. Defaults to 0.1.
-        """
-        super().__init__()
-
-        self.k = k
-        self.c = c
-    
-    def forward(self, x):
-        return self.k * nn.functional.tanh(self.c* x / 2)
 
 
 class SpectralConvergengeLoss(nn.Module):
@@ -112,3 +82,27 @@ class GroupDelayLoss(nn.Module):
         grad_phase = torch.diff(phase, dim=2)
         grad_target = torch.diff(target_phase, dim=2)
         return F.l1_loss(grad_phase, grad_target)
+
+class MultiResolutionLoss(nn.Module):
+    def __init__(self, n_ffts: list = [256, 512, 1025, 2096]):
+        super().__init__()
+        self.n_ffts = n_ffts
+        self.transforms = nn.ModuleList([
+            T.Spectrogram(
+                n_fft=n_fft,
+                power=1.0,
+                normalized=True,
+                center=False
+            )
+            for n_fft in n_ffts])
+        
+    def forward(self, clean, enhanced):
+        losses = []
+        for transform in self.transforms:
+            transform.window = transform.window.to(clean.device)
+            
+            S_clean = transform(clean).log1p()
+            S_enhanced = transform(enhanced).log1p()
+            losses.append(F.l1_loss(S_clean, S_enhanced))
+            
+        return torch.mean(torch.stack(losses))
--- utils/phase_rec.py
+++ utils/phase_rec.py
@@ -5,10 +5,27 @@ class PhaseCorrector(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(2, 16, kernel_size=3, padding=1),
+            nn.Conv2d(2, 16, kernel_size=3, padding=1,),
             nn.ReLU(),
             nn.Conv2d(16, 1, kernel_size=3, padding=1),
-            nn.Tanh()  # [-1, 1] для коррекции фазы
+            nn.Tanh()  
+        )
+        
+    def forward(self, mag, noisy_phase):
+        phase_diff = self.conv(torch.cat([mag, noisy_phase], dim=1))
+        return noisy_phase + phase_diff
+    
+
+class PhaseCorrectorDilation(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(2, 16, kernel_size=(3,5), padding='same'),
+            nn.ELU(),
+            nn.Conv2d(16, 16, kernel_size=(3,3), padding='same', dilation=(2,1)),
+            nn.ELU(),
+            nn.Conv2d(16, 1, kernel_size=3, padding='same'),
+            nn.Tanh()
         )
         
     def forward(self, mag, noisy_phase):
